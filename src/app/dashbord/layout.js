402253e56a0d285dc167bbbdcf27ac580fa81a32
HELAL-UDDIN-BISWAS <mdhelal6775@gmail.com>
2024-01-17 19:00:44 +0600

    add chart

--- src/app/dashbord/layout.js
+++ src/app/dashbord/layout.js
@@ -3,12 +3,14 @@ import React from 'react';
 
 const Dashlayout = ({ children }) => {
     return (
-        <div className='flex'>
-            <div>
-                <ul>
-                    <li><Link href="/">Home</Link></li>
-                    <li><Link href="/dashbord/hoome">dashbord H</Link></li>
-                    <li><Link href="/dashbord/missin">missin</Link></li>
+        <div className='flex gap-3 max-w-6xl mx-auto'>
+            <div className='bg-slate-300 w-1/5 rounded h-[90vh]'>
+                <ul className='my-2 gap-3'>
+                    <li className='my-3'><Link href="/dashbord/hoome"><button class="btn w-full btn-outline btn-secondary">Acount</button></Link></li>
+                    <li className='my-3'><Link href="/dashbord"><button class="btn w-full btn-outline btn-secondary">Home</button></Link></li>
+                    <li className='my-3'><Link href="/dashbord/missin"><button class="btn w-full btn-outline btn-secondary">Missin</button></Link></li>
+                    <hr></hr>
+                    <li className='my-3'><Link href="/"><button class="btn w-full btn-outline btn-secondary">Home</button></Link></li>
                 </ul>
             </div>
             {children}
--- src/app/layout.js
+++ src/app/layout.js
@@ -10,45 +10,47 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
-  const nave=<>
-  <li><Link href="/">Home</Link></li>
-  <li><Link href="/about">about</Link></li>
-  <li><Link href="/dashbord">Dashbord</Link></li>
+  const nave = <>
+    <li><Link href="/">Home</Link></li>
+    <li><Link href="/about">about</Link></li>
+    <li><Link href="/dashbord">Dashbord</Link></li>
   </>
   return (
     <html lang="en">
-   
+
       <body className={inter.className}>
-        <div>
-        <div className="navbar bg-base-100">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-      </div>
-      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-      {
-        nave
-      }
-      </ul>
-    </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-    {
-      nave
-    }
-    </ul>
-  </div>
-  <div className="navbar-end">
-    <a className="btn">Button</a>
-  </div>
-</div>
-        {children}
+        <div className='max-w-7xl mx-auto'>
+          <div className="navbar bg-base-100">
+            <div className="navbar-start">
+              <div className="dropdown">
+                <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                </div>
+                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                  {
+                    nave
+                  }
+                </ul>
+              </div>
+              <a className="btn btn-ghost text-xl">daisyUI</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+              <ul className="menu menu-horizontal px-1">
+                {
+                  nave
+                }
+              </ul>
+            </div>
+            <div className="navbar-end">
+              <a className="btn">Button</a>
+            </div>
+          </div>
+          <div>
+            {children}
+          </div>
         </div>
-     
-     </body>
+
+      </body>
     </html>
   )
 }
